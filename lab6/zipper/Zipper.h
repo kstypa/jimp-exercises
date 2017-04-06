640712d5a85d9050f9c0dd443089e1b65b6dc412
Karol Stypa <bury09@gmail.com>
2017-04-06 15:32:43 +0200

    Updated Zipper

--- lab6/zipper/Zipper.h
+++ lab6/zipper/Zipper.h
@@ -5,10 +5,27 @@
 #ifndef JIMP_EXERCISES_ZIPPER_H
 #define JIMP_EXERCISES_ZIPPER_H
 
+#include <vector>
+#include <string>
 
-class Zipper {
+namespace datastructures {
+    class Iterator {
+    public:
 
-};
+    private:
+        bool operator!=(Iterator);
+        Iterator &operator++();
+        std::pair<std::string, int> operator*();
+    };
+
+    class Zipper {
+    public:
+
+        static std::pair zip(std::vector<std::string>, std::vector<int>);
+    private:
+
+    };
+}
 
 
 #endif //JIMP_EXERCISES_ZIPPER_H
